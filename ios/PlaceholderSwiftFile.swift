5a26742786b8c2ac3b9ac2ecd3c3557089c11c80
Jarred Sumner <jarred@jarredsumner.com>
2020-02-14 03:38:23 -0800

    Example repo

---
+++ ios/PlaceholderSwiftFile.swift
@@ -0,0 +1,9 @@
+//
+//  PlaceholderSwiftFile.swift
+//  mediaclipboardexample
+//
+//  Created by Jarred WSumner on 2/14/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import Foundation
